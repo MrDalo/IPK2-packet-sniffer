141985461e419525449c0d800c66860b3ba7d6bc
MrDalo <dadokralik@gmail.com>
2022-04-13 15:58:31 +0200

    Create funcion ArgumentProcessing

--- ipk-sniffer.c
+++ ipk-sniffer.c
@@ -36,95 +36,10 @@ static struct option long_options[] =
     {NULL, 0, NULL, 0}
 };
 
-
-
-int main(int argc, char *argv[])
+void ArgumentProcessing(struct argFields *argumentsOfprogram, int argc, char *argv[] )
 {
-    
-    int opt;
-    struct argFields argumentsOfprogram = {NULL, -1, 1, false, false, false, false};
 
 
-    // :: means optional argument, : means required
-    // while((opt = getopt_long(argc, argv, "i::p:tun:", long_options, NULL)) != -1)
-    // {
-    //     char *errorIndex = NULL;
-    //     //printf("%c, %s, %d\n",opt, optarg, optind );
-    //     switch (opt)
-    //     {
-    //         case 'i':
-    //             argumentsOfprogram.interface = optarg;
-    //             break;
-    //         case 'p':
-    //             printf("%s\n", optarg);
-    //             if (optarg[0] == '-')
-    //             {
-    //                 fprintf(stderr, "Invalid port argument\n");
-    //                 exit(2);
-    //             }
-    //             argumentsOfprogram.port = strtol(optarg,&errorIndex,10);
-               
-    //             // printf("port: %s\n", errorIndex);
-    //             // if (errorIndex != NULL)
-    //             // {
-    //             //     fprintf(stderr, "Invalid 1port argument\n");
-    //             //     exit(2);
-    //             // }
-               
-    //             if (argumentsOfprogram.port < 0)
-    //             {
-    //                 fprintf(stderr, "Invalid port number\n");
-    //                 exit(2);
-    //             }
-
-    //             break;
-    //         case 't':
-    //             argumentsOfprogram.t = true;
-    //             break;
-    //         case 'u':
-    //             argumentsOfprogram.u = true;
-    //             break;
-    //         case 'n':
-    //             if (optarg[0] == '-')
-    //             {
-    //                 fprintf(stderr, "Invalid Number of packets argument\n");
-    //                 exit(2);
-    //             }
-    //             // argumentsOfprogram.n = strtol(optarg,&errorIndex,10);
-
-    //             // if (errorIndex != NULL)
-    //             // {
-    //             //     fprintf(stderr, "Invalid Number1 of packets argument\n");
-    //             //     exit(2);
-    //             // }
-
-
-    //             if (argumentsOfprogram.n < 1)
-    //             {
-    //                 fprintf(stderr, "Invalid Number of packets number\n");
-    //                 exit(2);
-    //             }
-    //             break;
-    //         case 'a':
-    //             argumentsOfprogram.a = true;
-    //             break;
-    //         case 'c':
-    //             argumentsOfprogram.c= true;
-    //             break;
-    //         case '?':
-    //             fprintf(stderr, "Unknown program argument\n");
-    //             break;
-            
-
-    //         default:
-    //             break;
-    //     }
-
-
-
-
-    // }
-    
     for(int i = 1; i < argc; i++)
     {
     
@@ -133,7 +48,7 @@ int main(int argc, char *argv[])
             i++;
             if( i < argc && argv[i][0] != '-')
             {
-                argumentsOfprogram.interface = argv[i];
+                argumentsOfprogram->interface = argv[i];
             }
             else
                 i--;
@@ -143,30 +58,30 @@ int main(int argc, char *argv[])
         {
             i++;
             if (i < argc)
-                argumentsOfprogram.port = atoi(argv[i]);
+                argumentsOfprogram->port = atoi(argv[i]);
             
         }
         else if(!strcmp(argv[i],"--tcp") || !strcmp(argv[i], "-t"))
         {
-            argumentsOfprogram.tcp = true;
+            argumentsOfprogram->tcp = true;
         }
         else if(!strcmp(argv[i],"--udp") || !strcmp(argv[i], "-u"))
         {
-            argumentsOfprogram.udp = true;
+            argumentsOfprogram->udp = true;
         }
         else if(!strcmp(argv[i],"--arp"))
         {
-            argumentsOfprogram.arp = true;
+            argumentsOfprogram->arp = true;
         }
         else if(!strcmp(argv[i],"--icmp"))
         {
-            argumentsOfprogram.icmp = true;
+            argumentsOfprogram->icmp = true;
         }
         else if(!strcmp(argv[i],"-n"))
         {
             i++;
             if (i < argc)
-                argumentsOfprogram.n = atoi(argv[i]);
+                argumentsOfprogram->n = atoi(argv[i]);
         }
         else 
         {
@@ -178,7 +93,17 @@ int main(int argc, char *argv[])
         
     }
     
-    printf("%s, %d, %d, %d, %d, %d, %d\n", argumentsOfprogram.interface, argumentsOfprogram.port, argumentsOfprogram.n, argumentsOfprogram.tcp, argumentsOfprogram.udp, argumentsOfprogram.arp, argumentsOfprogram.icmp);
+}
+
+
+int main(int argc, char *argv[])
+{
+    
+    int opt;
+    struct argFields argumentsOfprogram = {NULL, -1, 1, false, false, false, false};
+
+    ArgumentProcessing(&argumentsOfprogram, argc, argv);
+    //printf("%s, %d, %d, %d, %d, %d, %d\n", argumentsOfprogram.interface, argumentsOfprogram.port, argumentsOfprogram.n, argumentsOfprogram.tcp, argumentsOfprogram.udp, argumentsOfprogram.arp, argumentsOfprogram.icmp);
 
     return 0;
 }
